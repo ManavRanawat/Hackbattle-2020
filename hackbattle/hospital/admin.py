abd20ced96c590db6915135199490b2dedf48d03
Jash Gandhi <g.jash9900@gmail.com>
2020-12-19 11:08:27 +0530

    Updated models.py on hospital and users

--- hackbattle/hospital/admin.py
+++ hackbattle/hospital/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Hospital,Post
+from .models import Hospital,Post,Speciality,Appointment
 # Register your models here.
 admin.site.register(Hospital)
 admin.site.register(Post)
+admin.site.register(Speciality)
+admin.site.register(Appointment)
